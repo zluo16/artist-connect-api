206ba550f9d514668b77e349598d6f66f1c6f3c4
Mike <zluo@alumni.berklee.edu>
2017-08-16 14:54:01 -0400

    created dumby job descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-mike = User.create(first_name: 'Mike', last_name: 'Luo')
-vera = User.create(first_name: 'Vera', last_name: 'Zago')
-dave = User.create(first_name: 'Dave', last_name: 'Matthews')
+# mike = User.create(first_name: 'Mike', last_name: 'Luo')
+# vera = User.create(first_name: 'Vera', last_name: 'Zago')
+# dave = User.create(first_name: 'Dave', last_name: 'Matthews')
+#
+# band = Organization.create(name: 'Dave Matthews Band', organization_type: 'Band')
+# Ownership.create(owner: dave, owned_org: band, relation: 'Band Leader')
+#
+# house = Location.create(locatable: mike, address: '2066 Crist Dr.', city: 'Los Altos', state: 'CA', zipcode: '94024')
+#
+# Education.create(user: mike, school_name: 'Berklee College of Music')
+# Experience.create(user: mike, company_name: 'Google', title: 'CEO')
+# Skill.create(user: mike, skill: 'Being Awesome')
+#
+# guitar = JobPosting.create(organization: band, title: 'Guitarist')
+# Application.create(user: mike, job_posting: guitar)
 
-band = Organization.create(name: 'Dave Matthews Band', organization_type: 'Band')
-Ownership.create(owner: dave, owned_org: band, relation: 'Band Leader')
-
-house = Location.create(locatable: mike, address: '2066 Crist Dr.', city: 'Los Altos', state: 'CA', zipcode: '94024')
-
-Education.create(user: mike, school_name: 'Berklee College of Music')
-Experience.create(user: mike, company_name: 'Google', title: 'CEO')
-Skill.create(user: mike, skill: 'Being Awesome')
-
-guitar = JobPosting.create(organization: band, title: 'Guitarist')
-Application.create(user: mike, job_posting: guitar)
+JobPosting.all.each do |job|
+  if job.description == nil
+    job.update(description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+  end
+end
